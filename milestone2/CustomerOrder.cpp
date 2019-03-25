82c861227a625c769fea47c2e5e9708aea45c36f
rivalDevYP <yathavanparamesh1998@gmail.com>
2019-03-25 12:44:30 -0400

    milestone 3 update

--- milestone2/CustomerOrder.cpp
+++ milestone2/CustomerOrder.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <algorithm>
 #include "CustomerOrder.h"
 #include "Utilities.h"
 
@@ -10,27 +11,50 @@ namespace sict
     {
         customerName.clear();
         assembledProduct.clear();
-        components.clear();
+        ItemInfo.itemName.clear();
+        ItemInfo.serialNumber = 0;
+        
     }
 
     CustomerOrder::CustomerOrder(const std::string& incomingStr)
     {
-        size_t positionOfFirstDelim = incomingStr.find_first_of(helperObject.getDelimiter);
-        size_t positionOfSecondDelim{0};
-        size_t positionOfThirdDelim{0};
-        size_t beginningOfStr{0};
+        size_t beginningOfStr = 0;
+		size_t positionOfFirstDelim = 0;
+		size_t positionOfSecondDelim = 0;
+		size_t positionOfThirdDelim = 0;
+        static int counter = 0;
 
-        customerName = helperObject.extractToken(incomingStr, beginningOfStr);
-        assembledProduct = helperObject.extractToken(incomingStr, positionOfFirstDelim);
-        size_t pos;
-        while ((pos = incomingStr.find(helperObject.getDelimiter())) != std::string::npos)
+		positionOfFirstDelim = incomingStr.find(helperObject.getDelimiter());
+		std::string newStr = incomingStr.substr(positionOfFirstDelim + 1, incomingStr.length());
+		positionOfSecondDelim = newStr.find_first_of(helperObject.getDelimiter());	
+		std::string newStr1 = newStr.substr(positionOfSecondDelim + 1, newStr.length());
+		positionOfThirdDelim = incomingStr.rfind(helperObject.getDelimiter());
+
+        try
         {
-            components.push_back(helperObject.extractToken(incomingStr,pos));
-            incomingStr.erase(0, pos + 1);
+            this->customerName = helperObject.extractToken(incomingStr,positionOfFirstDelim);
+            this->assembledProduct = helperObject.extractToken(newStr,beginningOfStr);
+            
+            
         }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+        
     }
 
     CustomerOrder::~CustomerOrder()
+    {
+
+    }
+
+    CustomerOrder::CustomerOrder(CustomerOrder&& incomingObj)
+    {
+
+    }
+
+    CustomerOrder& CustomerOrder::operator=(CustomerOrder&& incomingObj)
     {
 
     }
--- milestone2/CustomerOrder.h
+++ milestone2/CustomerOrder.h
@@ -9,17 +9,28 @@
 
 namespace sict
 {
+    const unsigned int maxNumOfItems = 10;
+
     class CustomerOrder
     {
         Utilities helperObject;
+        ItemSet itemSetObject;
         std::string customerName;
         std::string assembledProduct;
-        std::deque<std::string> components;
+        
+        struct
+        {
+            std::string itemName;
+            long serialNumber;
+            bool filled;
+        } ItemInfo[maxNumOfItems];
 
     public:
         CustomerOrder();
         CustomerOrder(const std::string& incomingStr);
         ~CustomerOrder();
+        CustomerOrder (CustomerOrder&& incomingObj);
+        CustomerOrder& operator=(CustomerOrder&& incomingObj);
         void fillItem(ItemSet& item, std::ostream& os);
         bool isFilled() const;
         bool isItemFilled(const std::string& item) const;
