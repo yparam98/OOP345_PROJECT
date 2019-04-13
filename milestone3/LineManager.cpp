d7920417e8b7456fce9707a52318d891a2677240
rivalDevYP <yathavanparamesh1998@gmail.com>
2019-04-13 15:35:53 -0400

    PAST OVERDUE

--- milestone3/LineManager.cpp
+++ milestone3/LineManager.cpp
@@ -44,29 +44,29 @@ namespace sict
 
 	bool LineManager::run(std::ostream &os)
 	{
-		sict::CustomerOrder* newOrder = new CustomerOrder();
+		sict::CustomerOrder* newOrder = new CustomerOrder(); // creating new CustomerOrder object 
   
-		if (!myCustomerOrder.back().isFilled()) 
+		if (!myCustomerOrder.back().isFilled()) // if the customer order on back of queue not filled 
 		{
-			myStation.at(startingVal)->operator+=(std::move(myCustomerOrder.back())); 
+			myStation.at(startingVal)->operator+=(std::move(myCustomerOrder.back())); // move the customer order at back of queue to the station at starting position
 
-			for (auto stationIterator = myStation.begin(); stationIterator != myStation.end(); stationIterator++) 
+			for (auto stationIterator = myStation.begin(); stationIterator != myStation.end(); stationIterator++) // cycle through every station
 			{
-				(*stationIterator)->fill(os);
+				(*stationIterator)->fill(os); // fill station positioned at every incrementing iteration
 			}
 
-			myCustomerOrder.pop_back();		
-			size_t temp{0};
+			myCustomerOrder.pop_back(); // remove the station at back of the queue
+			size_t temp{0}; // creating size_t variable called temp, initialized to 0
 
-			for (auto myIndexesIterator = myIndexes.begin(); myIndexesIterator != myIndexes.end(); myIndexesIterator++)
+			for (auto myIndexesIterator = myIndexes.begin(); myIndexesIterator != myIndexes.end(); myIndexesIterator++) // cycle through every every element in myIndexes container
 			{
-				temp++;
+				temp++; // increment temp variable by one for every iteration
 
-				if (myStation.at(*myIndexesIterator)->hasAnOrderToRelease())
+				if (myStation.at(*myIndexesIterator)->hasAnOrderToRelease()) // if the station at position of current myIndex has an order to release
 				{
-					if (*myIndexesIterator == myIndexes.back())
+					if (*myIndexesIterator == myIndexes.back()) // if current station is not the last station
 					{
-						if (myStation.at(*myIndexesIterator)->pop(*newOrder))
+						if (myStation.at(*myIndexesIterator)->pop(*newOrder)) // moves the first order in station's customer order queue to newOrder, and returns true if the station filled its part of the order
 						{
 							*myOutputStream << " --> " 
 								<< newOrder->getNameProduct() 
@@ -76,8 +76,8 @@ namespace sict
 								<< "Completed Set" 
 								<< std::endl;
 
-							completeOrders.push_back(std::move(*newOrder));
-							delete newOrder;
+							completeOrders.push_back(std::move(*newOrder)); // move order to completed queue
+							delete newOrder; // deallocate memory for newOrder
 						}
 						else
 						{
@@ -89,13 +89,13 @@ namespace sict
 								<< "Incomplete Set" 
 								<< std::endl;
 
-							incompleteOrders.push_back(std::move(*newOrder));
-							delete newOrder;
+							incompleteOrders.push_back(std::move(*newOrder)); // if order is not filled, move to incomplete queue
+							delete newOrder; // deallocate memory for newOrder
 						}
 					}
 					else
 					{
-						if (myStation.at(*myIndexesIterator)->pop(*newOrder))
+						if (myStation.at(*myIndexesIterator)->pop(*newOrder)) // if current station is not the last station
 						{
 							*myOutputStream << " --> " 
 								<< newOrder->getNameProduct() 
@@ -105,14 +105,14 @@ namespace sict
 								<< myStation.at(myIndexes.at(temp))->getName() 
 								<< std::endl;
 								
-							myStation.at(myIndexes.at(temp))->operator+=(std::move(*newOrder));
-							delete newOrder;
+							myStation.at(myIndexes.at(temp))->operator+=(std::move(*newOrder)); // move the order to the next station
+							delete newOrder; // deallocates memory for newOrder
 						}
 					}
 				} 
 			}
-			processedOrdersCount++;			
+			processedOrdersCount++;	// increments the count for processed order
 		}
-		return processedOrdersCount == sizeOfOrders ? true : false;
+		return processedOrdersCount == sizeOfOrders ? true : false; // returns true of false, depending if the number of processed orders match the number of original orders
 	}
 }
